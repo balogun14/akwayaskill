93f58140c777c5284b7467322585dce85fc6276f
balogun14 <awwalbalogun06@gmail.com>
2023-07-06 10:11:06 +0100

    updated the hero component

--- src/app/hero-section/hero-section.component.ts
+++ src/app/hero-section/hero-section.component.ts
@@ -6,26 +6,24 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./hero-section.component.css']
 })
 export class HeroSectionComponent implements OnInit {
-
   hours!: number;
   minutes!: number;
   seconds!: number;
 
   ngOnInit() {
-    const countDownDate = new Date("Jul 2, 2023 23:59:00");
-    this.updateCountDown(countDownDate);
+    const countDownDate = new Date();
+    countDownDate.setHours(23);
+    countDownDate.setMinutes(59);
+    countDownDate.setSeconds(0);
 
-    setInterval(() => {     
-      var customNow = new Date(" 22:51:00").getTime();
-      this.updateCountDown(countDownDate);
+    setInterval(() => {
+      const now = new Date().getTime();
+      const distance = countDownDate.getTime() - now;
+
+      this.hours = Math.floor(distance / (1000 * 60 * 60));
+      this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+      this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
     }, 1000);
-  }
-  updateCountDown(countDownDate: Date) {
-    const customNow = new Date().getTime();
-    const distance = countDownDate.getTime() - customNow;
-    this.hours = Math.floor(distance / (1000 * 60 * 60));
-    this.minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-    this.seconds = Math.floor((distance % (1000 * 60)) / 1000);
   }
 }
 
